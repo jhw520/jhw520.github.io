f8eea452a77fdee27cec53c22489c6c2c95beefc
jhw <1121376943@qq.com>
2018-03-22 04:07:48 -0700

    add Single necklace table

---
+++ danlianbiao.c
@@ -0,0 +1,41 @@
+/*****************************************************
+njit 2016-2017,     Ltd. 
+File name：
+Author jhw    Version:0.1    Date: 
+Description：
+Funcion List: 
+*****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct student
+{
+	int score;
+	struct student *next;
+}Stu;
+
+
+
+int main()
+{
+	Stu *head, *q, *p;
+	head = (Stu*)malloc(sizeof(Stu));
+	head->next = NULL;
+
+	for(int i=0; i<10; i++)
+	{
+		p = (Stu*)malloc(sizeof(Stu));
+		p->score = i;
+		p->next = head->next;
+		head->next = p;
+	}
+	q = head->next; 
+	
+	while(q)
+	{
+		printf("%d\n",q->score);
+		q = q->next;
+	}
+    return 0;
+}
